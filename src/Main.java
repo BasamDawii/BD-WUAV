74a77f22d5c01ed6c1ee932749f318618510eda8
Marcelo Hani <90682271+Bigdeal99@users.noreply.github.com>
2023-04-27 14:26:55 +0200

    som data

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Application.launch();
+
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+
+        Parent root = loader.load(getClass().getResource("/Gui/Views/LoginView.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Ticketing System");
+        primaryStage.show();
     }
 }
