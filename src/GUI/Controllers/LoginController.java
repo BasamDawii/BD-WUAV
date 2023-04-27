2558c1565e474cd996a45a1d1d58d75c8a919b9c
BasamDawii <basa0033@easv365.dk>
2023-04-27 14:17:29 +0200

    LoginView

--- src/GUI/Controllers/LoginController.java
+++ src/GUI/Controllers/LoginController.java
@@ -1,4 +1,24 @@
 package GUI.Controllers;
 
-public class LoginController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+    @FXML
+    private TextField usernameTXT;
+    @FXML
+    private PasswordField passwordTXT;
+    @FXML
+    private Button loginTXT;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
