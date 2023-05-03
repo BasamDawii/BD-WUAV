0de9354d977d46bbe28a631058ddefcadc7e4cb0
BasamDawii <basa0033@easv365.dk>
2023-05-03 17:03:03 +0200

    nice

--- src/GUI/Controllers/TechnicianControllers/Editpic.java
+++ src/GUI/Controllers/TechnicianControllers/Editpic.java
@@ -3,21 +3,24 @@ package GUI.Controllers.TechnicianControllers;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
-
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Editpic {
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.FileChooser;
+
+public class Editpic implements Initializable {
     public Button saveimage;
     @FXML
     private Button exampleButton;
@@ -27,23 +30,23 @@ public class Editpic {
 
     private double mouseX;
     private double mouseY;
-    @FXML
-    private Slider imageSizeSlider;
 
     @FXML
     private ImageView uploadedImageView;
+    private boolean isDrawing = false;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
-    @FXML
-    private void initialize() {
-        // You can access and modify the exampleButton here.
     }
 
     public void uploadPictures() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select Pictures");
         fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Image Files", ".png", ".jpg", ".jpeg", ".gif"));
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif"));
+
         File picture = fileChooser.showOpenDialog(exampleButton.getScene().getWindow());
+
 
         if (picture != null) {
             try {
@@ -70,5 +73,40 @@ public class Editpic {
     }
 
     public void saveImage(ActionEvent event) {
+    }
+
+    public void drawButton(ActionEvent event) {
+        isDrawing = !isDrawing; // Toggle the drawing flag
+
+        if (isDrawing) {
+            // Enable drawing
+            imagePane.setOnMousePressed(this::startDrawing);
+            imagePane.setOnMouseDragged(this::continueDrawing);
+            imagePane.setOnMouseReleased(this::stopDrawing);
+        } else {
+            // Disable drawing
+            imagePane.setOnMousePressed(null);
+            imagePane.setOnMouseDragged(null);
+            imagePane.setOnMouseReleased(null);
+        }
+    }
+    private void startDrawing(MouseEvent event) {
+        double x = event.getX();
+        double y = event.getY();
+
+        Circle circle = new Circle(x, y, 5, Color.BLACK);
+        imagePane.getChildren().add(circle);
+    }
+
+    private void continueDrawing(MouseEvent event) {
+        double x = event.getX();
+        double y = event.getY();
+
+        Circle circle = new Circle(x, y, 5, Color.BLACK);
+        imagePane.getChildren().add(circle);
+    }
+
+    private void stopDrawing(MouseEvent event) {
+        // You can add any additional logic here if needed
     }
 }
--- src/GUI/Controllers/TechnicianControllers/TechnicianViewController.java
+++ src/GUI/Controllers/TechnicianControllers/TechnicianViewController.java
@@ -1,47 +1,31 @@
 package GUI.Controllers.TechnicianControllers;
 
-import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.FlowPane;
-import javafx.stage.FileChooser;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.util.List;
+
 
 public class TechnicianViewController {
 
-
-    public TextArea layoutDrawingField;
-
-    public void uploadPictures() {
-
-    }
-
-
+    @FXML
+    private Pane savedImageArea;
+    @FXML
+    private TextArea textFieldArea;
 
 
     public void uploadButton(ActionEvent event) {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Gui/Views/technician/Editpic.fxml"));
-            Parent editApictureView = fxmlLoader.load();
-
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Gui/Views/technician/Edit-pic.fxml"));
+            Parent editPictureView = fxmlLoader.load();
             Stage stage = new Stage();
             stage.setTitle("Edit a Picture");
-            stage.setScene(new Scene(editApictureView));
+            stage.setScene(new Scene(editPictureView));
             stage.show();
 
         } catch (IOException e) {
